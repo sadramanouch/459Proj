f31f94fa31578533f9eb58381b65361a5921b113
Franklin <Franklinau2@gmail.com>
2024-11-17 13:25:00 -0800

    Add IF and EE as outlier detection methods

--- main.py
+++ main.py
@@ -6,50 +6,69 @@ from outlier import outlierDetection
 import pandas as pd
 from feature_selection import mutual_info_selection
 from classification import performClassification
+from typing import Dict
+import argparse
 
-# fetch dataset
-wine_quality = fetch_ucirepo(id=186) 
+def parse_args() -> Dict[str, any]:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--outlier-method", type=str, default="LOF")
 
-# data (as pandas dataframes) 
-feature_headers = wine_quality.data.headers[:-2]
+    args = parser.parse_args()
+    return vars(args)
 
-# Data (as pandas DataFrames)
-print("Performing EDA")
-X = pd.DataFrame(wine_quality.data.features, columns=feature_headers)
-y = pd.Series(wine_quality.data.targets.squeeze(), name='quality')
-performEDA(X, y)
+def main() -> None:
+    args = parse_args()
 
-# Preprocess data
-print("Preprocessing Data")
-preprocessedData = preprocessData(wine_quality)
-X = preprocessedData["X"]
-y = preprocessedData["y"]
+    # fetch dataset
+    wine_quality = fetch_ucirepo(id=186) 
 
-# Outlier Detection
-print("Outlier Detection")
-outliersRemoved = outlierDetection(X, y)
-X = outliersRemoved["X"]
-y = outliersRemoved["y"]
+    # data (as pandas dataframes) 
+    feature_headers = wine_quality.data.headers[:-2]
 
-# Clustering with Mutual Information-selected features
-selected_features_mi = mutual_info_selection(X, y)
-X_mi = X[selected_features_mi]
+    # Data (as pandas DataFrames)
+    print("Performing EDA")
+    X = pd.DataFrame(wine_quality.data.features, columns=feature_headers)
+    y = pd.Series(wine_quality.data.targets.squeeze(), name='quality')
+    performEDA(X, y)
 
-print("Clustering with MI-selected features")
-clustering_results_mi = applyClustering(X_mi, save_path="clustering_plots")
 
-print("Clustering Evaluation Metrics with MI-selected features:")
-for clustering_method, metrics in clustering_results_mi.items():
-    print(f"\n{clustering_method} Results:")
-    for metric, score in metrics.items():
-        print(f"{metric}: {score}")
+    # Preprocess data
+    print("Preprocessing Data")
+    preprocessedData = preprocessData(wine_quality)
+    X = preprocessedData["X"]
+    y = preprocessedData["y"]
+    print(X)
 
-# Classification
-print("Performing Classification")
-classification_results = performClassification(X_mi, y, save_path="classification_results")
+    # Outlier Detection
+    print("Outlier Detection")
+    outlier_method = args["outlier_method"]
+    outliers_removed = outlierDetection(X, y, outlier_method)
+    X = outliers_removed["X"]
+    y = outliers_removed["y"]
+    print(X)
 
-# Print classification evaluation metrics
-for classifier_name, result in classification_results.items():
-    print(f"\n{classifier_name} Test Metrics:")
-    for metric_name, metric_value in result['test_metrics'].items():
-        print(f"{metric_name}: {metric_value}")
+    # Clustering with Mutual Information-selected features
+    selected_features_mi = mutual_info_selection(X, y)
+    X_mi = X[selected_features_mi]
+
+    print("Clustering with MI-selected features")
+    clustering_results_mi = applyClustering(X_mi, save_path="clustering_plots")
+
+    print("Clustering Evaluation Metrics with MI-selected features:")
+    for clustering_method, metrics in clustering_results_mi.items():
+        print(f"\n{clustering_method} Results:")
+        for metric, score in metrics.items():
+            print(f"{metric}: {score}")
+
+    # Classification
+    print("Performing Classification")
+    classification_results = performClassification(X_mi, y, save_path="classification_results")
+
+    # Print classification evaluation metrics
+    for classifier_name, result in classification_results.items():
+        print(f"\n{classifier_name} Test Metrics:")
+        for metric_name, metric_value in result['test_metrics'].items():
+            print(f"{metric_name}: {metric_value}")
+
+if __name__ == "__main__":
+    main()
--- outlier.py
+++ outlier.py
@@ -1,9 +1,20 @@
 from sklearn.neighbors import LocalOutlierFactor
+from sklearn.ensemble import IsolationForest
+from sklearn.covariance import EllipticEnvelope
 import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
 import pandas as pd
+import os
 
-def outlierDetection(X, y):
+def outlierDetection(X, y, outlier_method):
+    if outlier_method == "LOF":
+        return outlierDetectionLOF(X, y, outlier_method)
+    elif outlier_method == "IF":
+        return outlierDetectionIsolationForest(X, y, outlier_method)
+    elif outlier_method == "EE":
+        return outlierDetectionEllipticEnvelope(X, y, outlier_method)
+
+def outlierDetectionLOF(X, y, outlier_method):
     clf = LocalOutlierFactor(n_neighbors=20, contamination='auto')
     outlier_labels = clf.fit_predict(X)
 
@@ -13,11 +24,40 @@ def outlierDetection(X, y):
     X_pca = pd.DataFrame(X_pca, columns=["PC1", "PC2"])
     X_pca["outliers"] = outlier_labels
 
-    plotOutliers(X_pca)
+    plotOutliers(X_pca, outlier_method)
     removed_outliers = removeOutliers(X, y, outlier_labels)
     return {"X": removed_outliers["X"], "y": removed_outliers["y"]}
 
-def plotOutliers(X):
+def outlierDetectionIsolationForest(X, y, outlier_method):
+    clf = IsolationForest(random_state=0)
+    outlier_labels = clf.fit_predict(X)
+
+    # Reduce dimensionality to 2 for plotting
+    pca = PCA(n_components=2)
+    X_pca = pca.fit_transform(X)
+    X_pca = pd.DataFrame(X_pca, columns=["PC1", "PC2"])
+    X_pca["outliers"] = outlier_labels
+
+    plotOutliers(X_pca, outlier_method)
+    removed_outliers = removeOutliers(X, y, outlier_labels)
+    return {"X": removed_outliers["X"], "y": removed_outliers["y"]}
+
+def outlierDetectionEllipticEnvelope(X, y, outlier_method):
+    clf = EllipticEnvelope(random_state=0, contamination=0.1) 
+    clf.fit(X)
+    outlier_labels = clf.predict(X)
+
+    # Reduce dimensionality to 2 for plotting
+    pca = PCA(n_components=2)
+    X_pca = pca.fit_transform(X)
+    X_pca = pd.DataFrame(X_pca, columns=["PC1", "PC2"])
+    X_pca["outliers"] = outlier_labels
+
+    plotOutliers(X_pca, outlier_method)
+    removed_outliers = removeOutliers(X, y, outlier_labels)
+    return {"X": removed_outliers["X"], "y": removed_outliers["y"]}
+
+def plotOutliers(X, outlier_method):
     X_inliers = X[X["outliers"] == 1]
     X_outliers = X[X["outliers"] == -1]
 
@@ -28,8 +68,16 @@ def plotOutliers(X):
     plt.xlabel("Component 1")
     plt.ylabel("Component 2")
     plt.legend()
-    plt.title("Outlier Detection using Local Outlier Factor")
-    plt.show()
+    plt.title(f"Outlier Detection using {outlier_method}")
+
+    # Create a directory to save plots
+    plots_dir = "outlier_plots"
+    if not os.path.exists(plots_dir):
+        os.makedirs(plots_dir)
+    plt.tight_layout()
+    plt.savefig(os.path.join(plots_dir, outlier_method + ".png"))
+    plt.close()
+
 
 def removeOutliers(X, y, outlier_labels):
     inliers_mask = outlier_labels == 1
